e7b93a7cc88daee3970e0484a0cbb758a20bdc18
Tülay Sahin <celik.tuelay@gmail.com>
2020-09-17 15:23:40 +0200

    add my files

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ var separateline = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
 var repstring = "\n";
 var replacestriing = separateline.replace(/\//g, repstring);
 console.log(replacestriing);
+document.write("<hr>");
 
 // Advanced
 
@@ -72,3 +73,27 @@ var e = 42;
 
 var summ = a + b + c + d + e;
 console.log(summ);
+
+//challange 2
+document.write("Challange 2" + "<br>");
+
+var separateline =
+  "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
+var repstring = " ";
+var replacestriing = separateline.replace(/\$/g, repstring);
+document.write(replacestriing);
+document.write("<hr>");
+
+let iMax = 5;
+let jMax = 3;
+let myItems = [];
+let count = 1;
+for (let i = 0; i < iMax; i++) {
+  myItems[i] = [];
+  for (let j = 0; j < jMax; j++) {
+    myItems[i][j] = count;
+    count++;
+  }
+}
+console.log("myItems " + myItems);
+console.log("myItems[3][2] " + myItems[3][2]);
